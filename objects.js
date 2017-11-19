eb59a284f9ad1a1418674e8c5b0e5d16a11567fe
Kristen Leach <kristenleach24@gmail.com>
2017-11-19 18:27:00 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,2 +1,2 @@
-var playlist = new Objoect({Cohen: 'Everybody Knows', Young: 'Ohio', Zombies: 'Time of the Season'});
+var playlist = new Object({Cohen: 'Everybody Knows', Young: 'Ohio', Zombies: 'Time of the Season'});
 
