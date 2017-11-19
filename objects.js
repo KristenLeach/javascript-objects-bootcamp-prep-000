51fccba7f7558aee2d6fc0ef5bb15c8d2e334058
Kristen Leach <kristenleach24@gmail.com>
2017-11-19 18:15:26 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,1 +1,7 @@
 var playlist = {Leonard Cohen: 'Everybody Knows', Neil Young: 'Ohio', The Zombies: 'Time of the Season'}
+
+function updatePlaylist(obj, name, song){
+  playlist[Bob Dylan] = 'Fourth Time Around';
+  return playlist;
+}
+
