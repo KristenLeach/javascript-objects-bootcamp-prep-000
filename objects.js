0f5de18e8cee16af5a4f9b1bfd48b2fa806396de
Kristen Leach <kristenleach24@gmail.com>
2017-11-19 18:26:38 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,2 @@
 var playlist = new Objoect({Cohen: 'Everybody Knows', Young: 'Ohio', Zombies: 'Time of the Season'});
 
-function updatePlaylist(playlist, artistName, songTitle){
-  playlist[Bob Dylan] = 'Fourth Time Around';
-  return playlist;
-}
-
